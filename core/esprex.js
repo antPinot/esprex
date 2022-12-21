4f681a8ceca0640e12d96a7c97a003c67bf38c97
unknown <Saranthony@DESKTOP-VSCEOE9>
2022-12-21 13:04:58 +0100

    Factorize esprex.js

--- core/esprex.js
+++ core/esprex.js
@@ -1,18 +1,8 @@
 let routes = {};
 
 const esprexApp = (req, res) => {
-  switch (req.url) {
-    case '/':
-      routes[req.url](req, res);
-      break;
-    case '/contact':
-      routes[req.url](req,res)
-      break;
-    default:
-      res.end('Unrecognized URL')
-      break;
-  }
-  //res.end('HTTPS');
+  routes[req.url] ? routes[req.url](req,res) : res.end('Unrecognized URL')
+
 };
 
 esprexApp.get = (route, callback) => {
