510372d521cc28a32e4a16d254a8cf8a6588899f
unknown <Saranthony@DESKTOP-VSCEOE9>
2022-12-22 14:59:02 +0100

    add style and script

--- app.js
+++ app.js
@@ -9,6 +9,16 @@ app.get('/index', (req, res) => {
   res.sendFile(resolve('public' , 'index.html'))
 });
 
+app.get('/static/style.css', (req, res) => {
+  console.log(req.url)
+  res.sendFile(resolve('static' , 'style.css'))
+});
+
+app.get('/static/index.js', (req, res) => {
+  console.log(req.url)
+  res.sendFile(resolve('static' , 'index.js'))
+});
+
 app.get('/contact', (req, res) => {
   console.log(req.url)
   res.sendFile(resolve('public', 'contact.html'))
