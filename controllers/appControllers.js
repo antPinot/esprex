1c87b3ab204213fcf3f5289c99bd4841bd632eea
Anthony Pinot <pinot.anthony@gmail.com>
2022-12-23 17:37:56 +0100

    convert views from js to pug

--- controllers/appControllers.js
+++ controllers/appControllers.js
@@ -1,25 +1,24 @@
-const {resolve} = require('path')
-
 // Contrôleurs de l'application
 
 const indexController = (req, res) => {
     console.log(req.url)
-    res.send(require('../views/pages/index'))
+    res.render('pages/home.pug')
   }
 
 const contactController =  (req, res) => {
     console.log(req.url)
-    res.send(require('../views/pages/contact'))
+    res.render('pages/contact.pug')
   }
 
 const serviceController = (req, res) => {
     console.log(req.url)
-    res.send(require('../views/pages/services'))
+    res.render('pages/services.pug')
   }
 
 const utilisateurController = (req,res) => {
   console.log(req.url)
-  res.send(require('../views/pages/users'))
+  const {users} = require('../database/data.json')
+  res.render('pages/users.pug', {users})
 }
 
 const contactSubmitController = (req,res) => {
